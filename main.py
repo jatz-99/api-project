4df9ce06144c657f2d947d77680d87f848ca17b8
jatz-99 <creviajatz@gmail.com>
2026-09-11 10:01:25 +0000

    Add user creation endpoint

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 from pydantic import BaseModel
 from fastapi import FastAPI  
 app = FastAPI()
-print("THIS IS THE FILE I AM RUNNING")
-# @app.get("/fa/{no}")
-# def fa (no:int):
-#     result=1
-#     for i in range(1, no+1):
-#         result = result * i
-#     return result
+fake_db = {}
+@app.get("/fa/{no}")
+def fa (no:int):
+    result=1
+    for i in range(1, no+1):
+        result = result * i
+    return result
 
 
 
+user_id = int
 
 
-
-class User(BaseModel):
+class user(BaseModel):
   name: str
   age: int
   is_active: bool = True
 
 
 @app.post("/greet")
-def greet_user(User: User):
-    return {"message": "user created", "name": User.name, "age": User.age, "is_active": User.is_active}
+def greet_user(user: user, user_id: int):
+    return {"user_id": int ,"message": "user created", "name": User.name, "age": User.age, "is_active": User.is_active}
 
 
 
