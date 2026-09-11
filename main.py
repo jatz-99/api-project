5d90ed6e741db448e19d4671d800d74d3e448c35
jatz-99 <creviajatz@gmail.com>
2026-09-11 08:59:57 +0000

    add greet endpoint

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from unittest import result
-
+from pydantic import BaseModel
 from fastapi import FastAPI  
-
 app = FastAPI()
-@app.get("/fa/{no}")
-def fa (no:int):
-    result=1
-    for i in range(1, no+1):
-        result = result * i
-    return result
+print("THIS IS THE FILE I AM RUNNING")
+# @app.get("/fa/{no}")
+# def fa (no:int):
+#     result=1
+#     for i in range(1, no+1):
+#         result = result * i
+#     return result
+
+
+
+
+
+
+class User(BaseModel):
+  name: str
+  age: int
+  is_active: bool = True
+
+
+@app.post("/greet")
+def greet_user(User: User):
+    return {"message": "user created", "name": User.name, "age": User.age, "is_active": User.is_active}
+
+
+
